62fa10b8b70a2a183b70ce7b1557959f70bd6d66
LAGV <luisarturogonzalezvalencia@gmail.com>
2023-06-06 21:37:57 -0300

    asdasdd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -13,8 +12,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSlideToggleModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
